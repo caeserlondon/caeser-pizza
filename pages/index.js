a88479f8e56f3ba30818b2bb4ee27ae855e32822
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-22 12:35:03 +0100

    Updating the home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -10,6 +9,8 @@ export default function Home() {
 				<meta name='description' content='Created by Caeser Ibrahim' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
+
+			<div>home page</div>
 		</div>
 	)
 }
